5bdd75051a692c8f17d216f57a80febf39467d52
Biruk Worku <bworku1221@gmail.com>
2025-08-19 21:42:52 +0530

    Update Login.jsx

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./login.css";
+import "./Login.css";
 import { FaEnvelope, FaLock } from "react-icons/fa";
 import { API_PATHS } from "../../utils/ApiPaths";
 import axiosInstance from "../../utils/axiosInstance";
