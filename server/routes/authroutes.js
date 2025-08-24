86d9fb8266045b4e093832c92d39c3c2c446faa4
Biruk Worku <bworku1221@gmail.com>
2025-08-24 18:09:10 +0530

    fix registerUser error

--- server/routes/authroutes.js
+++ server/routes/authroutes.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const protect = require('../middlewares/authMiddleware');
-const { registerUser, loginUser, userProfile, sendOTP, verifyOTP} = require('../controllers/AuthController');
-const { verify } = require('jsonwebtoken');
+
+const {
+  registerUser,
+  loginUser,
+  userProfile,
+  sendOTP,
+  verifyOTP
+} = require('../controllers/AuthController');
 
 // POST /api/students
 router.post('/register', registerUser);
 router.post('/send-otp', sendOTP);
 router.post('/verify-otp', verifyOTP);
 router.post('/login', loginUser);
-router.get('/profile',protect, userProfile);
+router.get('/profile', protect, userProfile);
 
 module.exports = router;
